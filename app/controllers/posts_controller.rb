af37dd5ceec4366064b7218063a9887018b7ed95
Graham Rogers <graham@ophelos.com>
2022-02-18 15:33:14 +0000

    Create post with Turbo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,26 @@
 class PostsController < ApplicationController
   def create
     post = Post.create(params.require(:post).permit(:discussion_id, :text))
-    if post.valid?
-      redirect_to post.discussion
-    else
-      @discussion = post.discussion
-      @new_post = post
-      render "discussions/show", status: :unprocessable_entity
+    respond_to do |format|
+      format.html do
+        if post.valid?
+          redirect_to post.discussion
+        else
+          @discussion = post.discussion
+          @new_post = post
+          render "discussions/show", status: :unprocessable_entity
+        end
+      end
+      format.turbo_stream do
+        if post.valid?
+          @post = post
+          @new_post = Post.new(discussion_id: post.discussion_id)
+        else
+          @post = nil
+          @new_post = post
+          render status: :unprocessable_entity
+        end
+      end
     end
   end
 end
