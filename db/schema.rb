19206c2bcdfdcd3f2baa253105cd2bdcbd737376
Graham Rogers <graham@ophelos.com>
2022-02-18 13:04:57 +0000

    bin/rails generate model Post discussion:references text:string; bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_18_130239) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_18_130443) do
   create_table "discussions", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.integer "discussion_id", null: false
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["discussion_id"], name: "index_posts_on_discussion_id"
+  end
+
+  add_foreign_key "posts", "discussions"
 end
